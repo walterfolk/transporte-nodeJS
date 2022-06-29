ef9187ff63097f3cafa0e35bb9a7f68ce5793593
walterfolk <walter.folk@gmail.com>
2022-06-29 11:50:44 -0300

    migracion transporte

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var nosotrosRouter = require('./routes/nosotros');
+var serviciosRouter = require('./routes/servicios');
+var galeriaRouter = require('./routes/galeria');
+var novedadesRouter = require('./routes/novedades');
+var contactoRouter = require('./routes/contacto');
 
 var app = express();
 
@@ -20,7 +24,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/nosotros', nosotrosRouter);
+app.use('/servicios', serviciosRouter);
+app.use('/galeria', galeriaRouter);
+app.use('/novedades', novedadesRouter);
+app.use('/contacto', contactoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
