b3ff5223deb390fd8dbfea66a654819c6241f908
walterfolk <walter.folk@gmail.com>
2022-07-01 11:55:53 -0300

    funcionamiento envio de mail

--- routes/contacto.js
+++ routes/contacto.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
 
 router.get('/', function(req,res,next){
     res.render('contacto', {
@@ -7,5 +8,37 @@ router.get('/', function(req,res,next){
     });
 })
 
+router.post('/', async function (req,res,next) {
+    //console.log(req.body)
+    
+    var nombre = req.body.nombre;
+    var email = req.body.email;
+    var telefono = req.body.telefono;
+    var comentario = req.body.comentario;    
+
+    var obj = {
+        to:'walter@email.com',
+        subject:'Contacto desde la web',
+        html:nombre + " se contacto a traves de la web y quiere saber mas info a este correo: " + email +"<br> Su tel es: " + telefono + ". Su comentario es: " + comentario  
+    }
+
+    var transport = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASS
+        }
+      }); //finaliza transport
+
+      var info = await transport.sendMail(obj);
+      res.render('contacto',{
+        message: 'Mensaje enviado correctamente',
+        isContacto: true
+      })
+
+}) //finaliza router.post
+
+    
 
 module.exports = router;
