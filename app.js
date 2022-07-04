50deeea1deb0f93fdda358ddabd0f2d3a3936f58
walterfolk <walter.folk@gmail.com>
2022-07-04 12:00:24 -0300

    diseño del login

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var serviciosRouter = require('./routes/servicios');
 var galeriaRouter = require('./routes/galeria');
 var novedadesRouter = require('./routes/novedades');
 var contactoRouter = require('./routes/contacto');
+var loginRouter = require('./routes/admin/login');
 
 var app = express();
 
@@ -31,6 +32,8 @@ app.use('/servicios', serviciosRouter);
 app.use('/galeria', galeriaRouter);
 app.use('/novedades', novedadesRouter);
 app.use('/contacto', contactoRouter);
+app.use('/admin/login', loginRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
