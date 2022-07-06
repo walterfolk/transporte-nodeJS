3d8b29c6f00f468eba9b515bdff04fbcf636e4f9
walterfolk <walter.folk@gmail.com>
2022-07-06 12:00:19 -0300

    funcionamiento del login

--- routes/admin/login.js
+++ routes/admin/login.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var usuariosModel = require('./../../models/usuariosModel');
 
 router.get('/', function(req,res,next){
     res.render('admin/login',{
@@ -7,11 +8,23 @@ router.get('/', function(req,res,next){
     });
 })
 
-router.post('/', function (req,res,next){
+router.post('/', async function (req,res,next){
     
     try{
         console.log(req.body);
+        var usuario = req.body.usuario;
+        var password = req.body.password;
 
+        var data = await usuariosModel.getUserAndPassword(usuario,password);
+
+        if(data != undefined){
+            res.redirect("/admin/novedades")
+        }else {
+            res.render('admin/login', {
+                layout: 'admin/layout',
+                error:true
+            })
+        }
 
 
     }catch(error){
