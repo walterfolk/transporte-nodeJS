d520e128da1aae70ace456243a5bf6dd813f04f1
walterfolk <walter.folk@gmail.com>
2022-07-13 10:31:30 -0300

    agregar novedades

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -22,4 +22,27 @@ router.get('/agregar',(req,res,next) =>{
 
 })
 
+
+router.post('/agregar', async (req,res,next) =>{
+try{
+    if (req.body.titulo != "" &&req.body.subtitulo != "" && req.body.cuerpo != ""){
+        await novedadesModel.insertNovedades(req.body)
+        res.redirect('/admin/novedades')
+    } else {
+        res.render('admin/agregar',{
+            layout:'admin/layout',
+            error: true,
+            message: 'Todos los campos son requeridos'
+        })
+    }
+
+}catch(error){
+    console.log(error);
+    res.render('admin/agregar',{
+        layout: 'admin/layout',
+        error: true,
+        message: 'No se carga la novedad'
+    })
+}
+})
 module.exports = router; 
